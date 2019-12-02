3dd3ed9eeb76be2a2828d843cecff59bb1b5d5a5
xuhua <cxuhua@gmail.com>
2019-12-02 11:40:47 +0800

    'save'

--- netmsg.go
+++ netmsg.go
@@ -30,10 +30,6 @@ func (v NetPackage) ToMsgIO() (MsgIO, error) {
 		m = &MsgGetInv{}
 	case NT_GET_BLOCK:
 		m = &MsgGetBlock{}
-	//case NT_GET_HEADERS:
-	//	m = &MsgGetHeaders{}
-	//case NT_HEADERS:
-	//	m = &MsgHeaders{}
 	case NT_ERROR:
 		m = &MsgError{}
 	case NT_ALERT:
