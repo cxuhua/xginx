d76bca6242bd8e7ec040af3c0d77f5bc3e40ab97
xuhua <cxuhua@qq.com>
2020-05-13 17:59:49 +0800

    save

--- exec.go
+++ exec.go
@@ -1,6 +1,10 @@
 package xginx
 
-import "log"
+import (
+	"log"
+
+	lua "github.com/yuin/gopher-lua"
+)
 
 const (
 	//OptPushTxPool 当交易进入交易池
@@ -11,9 +15,22 @@ const (
 	OptPublishTx = 3
 )
 
+//初始化脚本状态机
+func initLuaEnv(slibs ...bool) *lua.LState {
+	opts := lua.Options{
+		CallStackSize:       64,
+		MinimizeStackMemory: true,
+		SkipOpenLibs:        len(slibs) == 0 || slibs[0],
+	}
+	l := lua.NewState(opts)
+	return l
+}
+
 //ExecScript 返回错误会不加入交易池或者不进入区块
 //执行之前已经校验了签名
 func (tx TX) ExecScript(bi *BlockIndex, opt int) error {
+	l := initLuaEnv()
+	defer l.Close()
 	id, _ := tx.ID()
 	log.Println(id, "ExecScript = ", opt, bi.Height())
 	return nil
@@ -22,6 +39,8 @@ func (tx TX) ExecScript(bi *BlockIndex, opt int) error {
 //ExecScript 执行签名交易脚本
 //执行之前签名已经通过
 func (sr mulsigner) ExecScript(bi *BlockIndex, wits WitnessScript, lcks LockedScript) error {
+	l := initLuaEnv()
+	defer l.Close()
 	id, _ := sr.tx.ID()
 	log.Println(id, "ExecScript Verify Sign", bi.Height())
 	return nil
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/stretchr/testify v1.5.1
 	github.com/syndtr/goleveldb v1.0.0
-	github.com/yuin/gopher-lua v0.0.0-20191220021717-ab39c6098bdb // indirect
+	github.com/yuin/gopher-lua v0.0.0-20191220021717-ab39c6098bdb
 	golang.org/x/sys v0.0.0-20200511232937-7e40ca221e25 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
