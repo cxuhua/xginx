7c31d904fd7dcd7c0e498f71066b6210e8f78449
xuhua <cxuhua@gmail.com>
2020-06-16 18:10:32 +0800

    save

--- client_test.go
+++ client_test.go
@@ -2,8 +2,12 @@ package xginx
 
 import (
 	"log"
+	"sync"
 	"testing"
 
+	sentinel "github.com/alibaba/sentinel-golang/api"
+	"github.com/alibaba/sentinel-golang/core/base"
+	"github.com/alibaba/sentinel-golang/core/flow"
 	"github.com/syndtr/goleveldb/leveldb/filter"
 )
 
@@ -11,4 +15,37 @@ func TestBloom(t *testing.T) {
 	b := filter.NewBloomFilter(10)
 	log.Println(b.Name())
 	b.NewGenerator()
+}
+
+func TestSentine(t *testing.T) {
+	sentinel.InitDefault()
+	_, err := flow.LoadRules([]*flow.FlowRule{
+		{
+			ID:                1,
+			Resource:          "name111",
+			MetricType:        flow.QPS,
+			Count:             15,
+			ControlBehavior:   flow.Throttling,
+			MaxQueueingTimeMs: 5000,
+		},
+	})
+	if err != nil {
+		// 加载规则失败，进行相关处理
+	}
+	num := 20
+	wg := sync.WaitGroup{}
+	wg.Add(num)
+	for i := 0; i < num; i++ {
+		go func(idx int) {
+			e, err := sentinel.Entry("name111", sentinel.WithTrafficType(base.Inbound))
+			if err != nil {
+				log.Println(idx, err)
+			} else {
+				log.Println(idx, "pass")
+				e.Exit()
+			}
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/cxuhua/xginx
 go 1.13
 
 require (
+	github.com/alibaba/sentinel-golang v0.3.0
 	github.com/cxuhua/gopher-lua v1.0.1
 	github.com/json-iterator/go v1.1.10
 	github.com/shopspring/decimal v1.2.0
